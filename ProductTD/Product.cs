ad871c76043dbc6a7a214bb5be180bbfe0a9c0a7
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-21 16:11:50 +0530

    Add project files.

---
+++ ProductTD/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductTD
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Product details : {Id}  {Name}  {Price}";
+        }
+    }
+
+}
---
+++ ProductTD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductTD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] products =
+            {
+                new Product{Id=1,Name="HP Laptop",Price=35000},
+                new Product{Id=2,Name="Dell Laptop",Price=35400},
+                new Product{Id=3,Name="Lenovo Laptop",Price=35050},
+                new Product{Id=4,Name="Sony Laptop",Price=34000},
+                new Product{Id=5,Name="Accer Laptop",Price=31000}
+
+            };
+            foreach (Product p in products)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+    }
+}
